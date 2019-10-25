23b524e1d984cc2f0b345bb632f9e1f33c152b78
daniel-duner <daniel_duner@hotmail.com>
2019-10-25 17:17:50 +0200

    First commit with homepage, navbar and slideshow added

--- plb/src/components/Slideshow.js
+++ plb/src/components/Slideshow.js
@@ -15,12 +15,12 @@ class Slideshow extends Component{
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <Image src={require("./img/home_slide_1.jpg")} fluid/>
+            <Image src={require("./img/home_slide_2.jpg")} fluid/>
           </div>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <Image src={require("./img/home_slide_1.jpg")} fluid/>
+            <Image src={require("./img/home_slide_3.jpg")} fluid/>
           </div>
         </div>
       </Fade>
