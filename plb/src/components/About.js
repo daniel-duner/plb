69470511b75a513b7067894df633b02e06687be4
jakmol@ug.kth.se <MmR-PZ-Q>
2019-10-27 15:53:18 +0100

    About us added

--- plb/src/components/About.js
+++ plb/src/components/About.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const About = () => {
-    return(
-        <div>About</div>
-    );
+import './slideShow.css';
+import Slideshow from './Slideshow';
+import Post from './Post';
+
+class About extends Component{
+    constructor (props){
+        super(props);
+        this.state = {
+            imageState: true,
+            fadeInCounter: 0,
+            fadeOutCounter: 1,
+            images: [require("./img/home_slide_1.jpg"),require("./img/home_slide_2.jpg"),require("./img/home_slide_3.jpg")],
+            fadeProperties: {
+                duration: 7000,
+                transitionDuration: 2000,
+                infinite: true,
+                indicators: false,
+                arrows: false
+            },
+
+        }
+    }
+
+    render(){
+        return (<div>
+            <Slideshow fadeProperties={this.state.fadeProperties} fadeImages={this.state.images} />
+            <Post/>
+            <Post/>
+        </div>);
+
+    };
+
 }
 
 export default About;
