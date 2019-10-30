dbebd5291b0b2618a75843d9c2eb69da1d680403
jakmol@ug.kth.se <MmR-PZ-Q>
2019-10-30 14:11:00 +0100

    text added for the picture

--- plb/src/components/Slideshow.js
+++ plb/src/components/Slideshow.js
@@ -1,17 +1,24 @@
 import React,{Component} from 'react';
 import { Fade } from 'react-slideshow-image';
 import Image from 'react-bootstrap/Image';
+import './font.css';
+import './slideShow.css';
  
 class Slideshow extends Component{
   render(){
+    var textStyle = {
+      position: 'absolute', 
+      top: '25%', 
+      left: '18%'
+    };
   return (
     <div className="slide-container">
       <Fade {...this.props.fadeProperties}>
         {this.props.fadeImages.map((picture) => 
           <div className="each-fade">
             <div className="image-container" >
-              <Image src={picture.toString()} fluid></Image>
-              {/* <h1 className="text-over-image">Text over image</h1> */}
+              <Image className="add-text" responsive src={picture.toString()} fluid></Image>
+              <h1 style={textStyle} className="text-image">FÖRETAGETS VÄRDERING</h1>
             </div>
           </div>
         )}
