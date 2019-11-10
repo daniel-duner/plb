14339c5515cf31534c29a54c09c50d6053e0cb03
daniel-duner <daniel_duner@hotmail.com>
2019-11-10 11:34:04 +0100

    merge conflict

--- plb/src/components/Post.js
+++ plb/src/components/Post.js
@@ -9,29 +9,29 @@ import './post.css';
 import './font.css';
 
 class Post extends Component {
- 
+
     render() {
         return (
             <Container fluid className="box-layout bottom-border">
                 <Row>
-                <Col className="align-center" style={{paddingTop: "50px", paddingLeft: "60px"}}>
-                <Carousel >
-                    {this.props.carouselImages.map((picture) => 
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={picture.toString()}
-                            />
-                        </Carousel.Item>
-                    )} 
-                </Carousel>
-                </Col>
-                <Col className="text-center">
-                <Container style={{padding: "50px 100px 50px 100px"}}>
-                <h1 className="post-standard-header">{this.props.title}</h1>
-                <p className="post-standard-text">{this.props.text}</p>
-                </Container>
-                </Col>
+                    <Col className="align-center" style={{ paddingTop: "50px", paddingLeft: "60px" }}>
+                        <Carousel >
+                            {this.props.carouselImages.map((picture) =>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={picture.toString()}
+                                    />
+                                </Carousel.Item>
+                            )}
+                        </Carousel>
+                    </Col>
+                    <Col className="text-center">
+                        <Container style={{ padding: "50px 100px 50px 100px" }}>
+                            <h1 className="post-standard-header">{this.props.title}</h1>
+                            <p className="post-standard-text">{this.props.text}</p>
+                        </Container>
+                    </Col>
                 </Row>
             </Container>
         );
