0dfc46cd1f431fbb939635922560372e083e3df8
daniel-duner <daniel_duner@hotmail.com>
2019-12-20 10:54:15 +0100

    reworked post and gogolemap

--- plb/src/components/fragments/GoogleMaps.js
+++ plb/src/components/fragments/GoogleMaps.js
@@ -3,24 +3,35 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+import '../../resources/css/googleMaps.css'
+import '../../resources/css/post.css';
+import '../../resources/css/font.css';
 
 class GoogleMaps extends Component {
   render() {
     return (
-      <Container fluid className={this.props.mobile?"":"box-layout bottom-border"}>
-        <Row>
-          <Col className="align-center" style={this.props.mobile?{}:{ paddingBottom: "0px", paddingLeft: "70px" }} md={this.props.mobile?0:6} >
-              <iframe  width={this.props.mobile?"100vw":"400px"} height={this.props.mobile?"100vw":"400px"} id="gmap_canvas" src="https://maps.google.com/maps?q=L%C3%A5ngholmsgatan%2030&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" />
+      <Container fluid className="box-layout bottom-border">
+        <Row class="align-center" style={{textAlign:"center"}}>
+          <Col style={this.props.mobile?{height:"600px",width:"600px"}:{ paddingBottom: "0px", paddingLeft: "70px" }} md={this.props.mobile?10:6} >
+              <iframe  
+              width={this.props.mobile?"100%":"400px"} 
+              height={this.props.mobile?"100%":"400px"} 
+              id="gmap_canvas" src="https://maps.google.com/maps?q=L%C3%A5ngholmsgatan%2030&t=&z=13&ie=UTF8&iwloc=&output=embed" 
+              frameborder="0" 
+              scrolling="no"
+              style={{display: "inlineBlock", padding:"50px"}}
+              />
           </Col>
-          <Col className="text-center" md={4}>
-            <Row>
-              <Container style={this.props.mobile?{}:{ padding: "50px 100px 0px 100px" }}>
+          <Col className="text-center" md={this.props.mobile?10:4} style={this.props.mobile?{}:{}}>
+              <Container style={{padding: "50px 100px 50px 100px" }}>
                 <h1 className="post-standard-header">{this.props.title}</h1>
                 <p className="post-standard-text">Långholmsgatan 30</p>
                 <p className="post-standard-text">117 33 Stockholm</p>
               </Container>
-            </Row>
           </Col>
+        </Row>
+        <Row>
+          
         </Row>
       </Container>
     );
--- plb/src/components/fragments/Post.js
+++ plb/src/components/fragments/Post.js
@@ -1,211 +1,76 @@
-import React, { Component } from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import React, { Component, Fragment } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
-
-import '../../resources/css/post.css';
-import '../../resources/css/font.css';
+import "../../resources/css/post.css";
+import "../../resources/css/font.css";
 
 class Post extends Component {
+  renderImage = () => {
+    return <Col className="align-center" style={{ paddingTop: "0px", paddingLeft: "60px" }}>
+                <img className="d-block w-100" src={this.props.carouselImages[0].toString()}/>
+           </Col>
+  }
+  renderList = () =>{
+    return <ul>{this.props.list.map(item => (<li className="post-standard-text">{item}</li>))}</ul>
+  }
+  renderHeader = () =>{
+      return <h1 className="post-standard-header">{this.props.title}</h1>
+  }
+  renderSigil = () =>{
+    return <a
+    className="post-standard-header"
+    href={this.props.sigil.href}
+    title={this.props.sigil.title}
+    target=" _blank">
+      <img src={this.props.sigil.src} />
+  </a>
+  }
+  renderText = ()  => {
+    return <p className="post-standard-text">{this.props.text}</p>
+  }
 
-    render() {
-        if (this.props.imagePosition == "left") {
-            if (this.props.list != null) {
-                return (
-                    <Container fluid className="box-layout bottom-border">
-                        <Row>
-                            <Col className="align-center" style={{ paddingTop: "0px", paddingLeft: "60px" }}>
-                                {/* <Carousel {...this.props.carouselSettings}> */}
-                                {/* <Carousel indicators='false' controls='false'>
-                                    {this.props.carouselImages.map((picture) =>
-                                        <Carousel.Item>
-                                            <img
-                                                className="d-block w-100"
-                                                src={picture.toString()}
-                                            />
-                                            <p className="post-standard-text">{this.props.imgText}</p>
-                                        </Carousel.Item>
-                                    )}
-                                </Carousel> */}
-                                <img
-                                    className="d-block w-100"
-                                    src={this.props.carouselImages[0].toString()}
-                                />
-                            </Col>
-                            <Col className="text-center">
-                                <Container style={{ padding: "50px 100px 50px 100px" }}>
-                                    <h1 className="post-standard-header">{this.props.title}</h1>
-                                    <ul >
-                                        {this.props.list.map((item) =>
-                                            <li className="post-standard-text">{item}</li>
-                                        )}
-                                    </ul>
-                                </Container>
-                            </Col>
-                        </Row>
-                    </Container >
-                )
-            } else {
-                return (
-                    <Container fluid className="box-layout bottom-border">
-                        <Row>
-                            <Col className="align-center" style={{ paddingTop: "0px", paddingLeft: "60px" }}>
-                                {/* <Carousel indicators='false'>
-                                    {this.props.carouselImages.map((picture) =>
-                                        <Carousel.Item>
-                                            <img
-                                                className="d-block w-100"
-                                                src={picture.toString()}
-                                            />
-                                            <p className="post-standard-text">{this.props.imgText}</p>
-                                        </Carousel.Item>
-                                    )}
-                                </Carousel> */}
-                                <img
-                                    className="d-block w-100"
-                                    src={this.props.carouselImages[0].toString()}
-                                />
-                            </Col>
-                            <Col className="text-center">
-                                <Container style={{ padding: "50px 100px 50px 100px" }}>
-                                    <h1 className="post-standard-header">{this.props.title}</h1>
-                                    <p className="post-standard-text">{this.props.text}</p>
-                                </Container>
-                            </Col>
-                        </Row>
-                    </Container>
-
-                );
-            }
-        } else if (this.props.imagePosition == "right") {
-            if (this.props.list != null) {
-                if (this.props.sigil != null) {
-                    return (
-                        <Container fluid className="box-layout bottom-border">
-                            <Row>
-                                <Col className="text-center">
-                                    <Container style={{ padding: "50px 100px 50px 100px" }}>
-                                        <h1 className="post-standard-header">{this.props.title}</h1>
-                                        <ul >
-                                            {this.props.list.map((item) =>
-                                                <li className="post-standard-text">{item}</li>
-                                            )}
-                                        </ul>
-                                        <a className="post-standard-header" href={this.props.sigil.href}
-                                            title={this.props.sigil.title} target=" _blank">
-                                            <img src={this.props.sigil.src}
-                                            />
-                                        </a>
-                                    </Container>
-                                </Col>
-                                <Col className="align-center" style={{ paddingTop: "0px", paddingLeft: "60px" }}>
-                                    {/* <Carousel indicators="false">
-                                        {this.props.carouselImages.map((picture) =>
-                                            <Carousel.Item>
-                                                <img
-                                                    className="d-block w-100"
-                                                    src={picture.toString()}
-                                                />
-                                                <p className="post-standard-text">{this.props.imgText}</p>
-                                            </Carousel.Item>
-                                        )}
-                                    </Carousel> */}
-                                    <img
-                                    className="d-block w-100"
-                                    src={this.props.carouselImages[0].toString()}
-                                />
-                                </Col>
-                            </Row>
-                        </Container>
-                    )
-                } else {
-                    return (
-                        <Container fluid className="box-layout bottom-border">
-                            <Row>
-                                <Col className="text-center">
-                                    <Container style={{ padding: "50px 100px 50px 100px" }}>
-                                        <h1 className="post-standard-header">{this.props.title}</h1>
-                                        <ul >
-                                            {this.props.list.map((item) =>
-                                                <li className="post-standard-text">{item}</li>
-                                            )}
-                                        </ul>
-                                    </Container>
-                                </Col>
-                                <Col className="align-center" style={{ paddingTop: "0px", paddingLeft: "60px" }}>
-                                    {/* <Carousel indicators="false">
-                                        {this.props.carouselImages.map((picture) =>
-                                            <Carousel.Item>
-                                                <img
-                                                    className="d-block w-100"
-                                                    src={picture.toString()}
-                                                />
-                                                <p className="post-standard-text">{this.props.imgText}</p>
-                                            </Carousel.Item>
-                                        )}
-                                    </Carousel> */}
-                                    <img
-                                    className="d-block w-100"
-                                    src={this.props.carouselImages[0].toString()}
-                                />
-                                </Col>
-                            </Row>
-                        </Container>
-                    )
-                }
-            } else {
-                return (
-                    <Container fluid className="box-layout bottom-border">
-                        <Row>
-                            <Col className="text-center">
-                                <Container style={{ padding: "50px 100px 50px 100px" }}>
-                                    <h1 className="post-standard-header">{this.props.title}</h1>
-                                    <p className="post-standard-text">{this.props.text}</p>
-                                </Container>
-                            </Col>
-                            <Col className="align-center" style={{ paddingTop: "0px", paddingLeft: "60px" }}>
-                                {/* <Carousel indicators="false">
-                                    {this.props.carouselImages.map((picture) =>
-                                        <Carousel.Item>
-                                            <img
-                                                className="d-block w-100"
-                                                src={picture.toString()}
-                                            />
-                                            <p className="post-standard-text">{this.props.imgText}</p>
-                                        </Carousel.Item>
-                                    )}
-                                </Carousel> */}
-                                <img
-                                    className="d-block w-100"
-                                    src={this.props.carouselImages[0].toString()}
-                                />
-                            </Col>
-                        </Row>
-                    </Container>
-                );
-            }
-        } else {
-            return (
-                <Container fluid className="box-layout bottom-border">
-                    <Row>
-                        <Col className="text-center">
-                            <Container style={{ padding: "50px 200px 50px 200px" }}>
-                                <h1 className="post-standard-header">{this.props.title}</h1>
-                                <p
-                                    className="post-standard-text"
-                                    style={{ fontSize: this.props.fontSize }}
-                                >{this.props.text}</p>
-                            </Container>
-                        </Col>
-                    </Row>
+  render() {
+      return(
+    <Container fluid className="box-layout bottom-border">
+        <Row>
+            {this.props.imagePosition == "left"?this.renderImage():""}
+            <Col className="text-center">
+                <Container style={{ padding: "50px 100px 50px 100px" }}>
+                    {this.renderHeader()}
+                    {this.props.list?this.renderList():this.renderText()}
+                    {!this.props.sigil?"":this.renderSigil()}
                 </Container>
-            );
-        }
-
-    };
+            </Col>
+            {this.props.imagePosition == "right"?this.renderImage():""}
+        </Row>
+    </Container> 
+  );
+}
 }
 
 export default Post;
 
 
+
+
+
+
+
+
+
+
+//Carousel element
+  {/* <Carousel {...this.props.carouselSettings}> */}
+                {/* <Carousel indicators='false' controls='false'>
+                    {this.props.carouselImages.map((picture) =>
+                        <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={picture.toString()}
+                        />
+                        <p className="post-standard-text">{this.props.imgText}</p>
+                            </Carousel.Item>
+                        )}
+                    </Carousel> */}
